e4cae30307106b2b4737dee2b8610db595a81b83
Dartzt1 <mixayuzuka20@gmail.com>
2023-03-29 19:54:52 -0500

    Plantilla attax

---
+++ Html-Conver/resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+<!-- Index -->
+@extends('layout/attax_index')
+@section('Index')
+
+<!-- 404 -->
+@extends('layout/attax_404')
+@section('404')
+
+<!-- about -->
+@extends('layout/attax_about')
+@section('about')
+
+<!-- blog-details -->
+@extends('layout/attax_blog-details')
+@section('blog_Details')
+
+<!-- blog -->
+@extends('layout/attax_blog')
+@section('Blog')
+
+<!-- case-study-details -->
+@extends('layout/attax_case-study-details')
+@section('study-details')
+
+<!-- case-study -->
+@extends('layout/attax_case-study')
+@section('case-study')
+
+<!-- contact -->
+@extends('layout/attax_contact')
+@section('contact')
+
+<!-- index 2 -->
+@extends('layout/attax_index-2')
+@section('index-2')
+
+<!-- index 3 -->
+@extends('layout/attax_index-3')
+@section('index-3')
+
+<!-- pricing -->
+@extends('layout/attax_pricing')
+@section('pricing')
+
+<!-- service-details -->
+@extends('layout/attax_service-details')
+@section('service-details')
+
+<!-- service -->
+@extends('layout/attax_service')
+@section('service')
+
+<!-- team details -->
+@extends('layout/attax_team-details')
+@section('team-details')
+
+<!-- team  -->
+@extends('layout/attax_team')
+@section('team')
+
+@endsection
