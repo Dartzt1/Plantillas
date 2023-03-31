c064fd3ba95ae9a83814e4840e0f47efbf3226aa
Dartzt1 <mixayuzuka20@gmail.com>
2023-03-31 13:01:26 -0500

    tercera subida

--- Html-Conver/resources/views/welcome-ecstacy.blade.php
+++ Html-Conver/resources/views/welcome-ecstacy.blade.php
@@ -1,7 +1,6 @@
-<!-- 404 -->
-@extends('layout/ecstacy/blog')
-@section('Blog')
+<!-- index  -->
+@extends('layout/ecstacy/index')
+@section('')
 
-<!-- index-2 -->
-@extends('layout/ecstacy/index-2')
-@section('index')
+
+
---
+++ Html-Conver/resources/views/welcome-magpie.blade.php
@@ -0,0 +1,29 @@
+<!-- author  -->
+@extends('layout/magpie/author')
+
+<!-- blog-details  -->
+@extends('layout/magpie/blog-details')
+
+<!-- blog -->
+@extends('layout/magpie/blog')
+
+<!-- cat-fashion -->
+@extends('layout/magpie/cat-fashion')
+
+<!-- 404 -->
+
+
+<!-- lifestyle -->
+@extends('layout/magpie/cat-lifestyle')
+
+<!-- tech -->
+@extends('layout/magpie/cat-tech')
+
+<!-- index-2 -->
+@extends('layout/magpie/index-2')
+
+<!-- index-3 -->
+@extends('layout/magpie/index-3')
+
+<!-- index-4 -->
+@extends('layout/magpie/index-4')
